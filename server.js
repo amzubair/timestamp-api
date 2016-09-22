1c3a374a72fdb21d936287df16c991c4f0b7b347
Mohammed Zubair <amzubair@live.com>
2016-09-22 16:50:18 +0000

    add timestamp api

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var app = express();
+var moment = require('moment');
+
+// get date or unix timestamp from user
+// parse the user data into date
+// return unix date and natural date to user in json format
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/timestamp/:datestring', function(req, res) {
+      var myDate;
+  if(/^\d{8,}$/.test(req.params.datestring)) {
+    myDate = moment(req.params.datestring, "X");
+  } else {
+    myDate = moment(req.params.datestring, "MMMM D, YYYY");
+  }
+
+  if(myDate.isValid()) {
+    res.json({
+      unix: myDate.format("X"),
+      natural: myDate.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+})
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile('Hello World!');
 });
+
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
